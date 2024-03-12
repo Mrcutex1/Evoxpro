95639722c0e129042198506ab4d1f6b91a25a87d
AMJID DADER <amjiddader@gmail.com>
2024-03-12 23:59:15 +0530

    Update help.py

--- DAXXMUSIC/plugins/bot/help.py
+++ DAXXMUSIC/plugins/bot/help.py
@@ -11,8 +11,8 @@ from strings import get_string, helpers
 from DAXXMUSIC.utils.stuffs.buttons import BUTTONS
 from DAXXMUSIC.utils.stuffs.helper import Helper
 
-#@app.on_message(filters.command(["help"]) & filters.private & ~BANNED_USERS)
-@app.on_message((filters.command(["help", "fuck"]) | filters.regex(r"@help") | filters.regex(r"!help")) & filters.private & ~filters.user(BANNED_USERS))
+@app.on_message(filters.command(["help"]) & filters.private & ~BANNED_USERS)
+#@app.on_message((filters.command(["help", "fuck"]) | filters.regex(r"@help") | filters.regex(r"!help")) & filters.private & ~filters.user(BANNED_USERS))
 @app.on_callback_query(filters.regex("settings_back_helper") & ~BANNED_USERS)
 async def helper_private(
     client: app, update: Union[types.Message, types.CallbackQuery]
